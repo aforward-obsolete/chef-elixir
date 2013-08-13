2d6df8c0dde98f9f179abed154cf78f51c2b832e
Andrew Forward <aforward@gmail.com>
2013-08-13 15:25:32 -0400

    Added support for elixir 0.10.2-dev and 0.10.1

--- recipes/from_tar.rb
+++ recipes/from_tar.rb
@@ -1,4 +1,6 @@
 
+# https://github.com/elixir-lang/elixir/archive/v#{node[:elixir][:version]}.tar.gz
+
 name = "elixir-#{node[:elixir][:version]}"
 filename = "#{name}.tar.gz"
 src_dir = "#{node[:elixir][:install_dir]}/elixir"
@@ -10,12 +12,17 @@ cookbook_file "#{node[:elixir][:install_dir]}/#{filename}" do
   mode '0644'
 end
 
+execute "remove old version" do
+  command "rm -rf #{src_dir}"
+  only_if { `iex -v | grep #{node[:elixir][:version]}`.strip.empty? }
+end
+
 execute "tar zxfv #{filename}" do
   command "tar zxfv #{filename}; mv ./#{name} ./elixir"
   cwd "#{node[:elixir][:install_dir]}"
   user 'root'
   group 'root'
-  not_if { File.exists?("#{src_dir}") }
+  not_if { File.exists?(src_dir) }
   notifies :run, "execute[elixir make]", :immediately
 end
 
