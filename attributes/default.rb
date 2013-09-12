edb538c4022e9b573d3158cb437701bda34d292b
Andrew Forward <aforward@gmail.com>
2013-09-12 12:44:23 -0400

    Added 0.10.2 and 0.10.3-dev

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default[:elixir][:install_dir] = "/usr/local/src"
 default[:elixir][:bin_dir] = "/usr/local/bin"
-default[:elixir][:version] = "0.10.2-dev" # use "git" for latest
+default[:elixir][:version] = "0.10.3-dev" # use "git" for latest
 
 # 5 are failing, should report
 default[:elixir][:run_tests] = false
